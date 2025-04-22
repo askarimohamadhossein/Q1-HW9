b5122a257f57566b0a20391d581722f886f84650
Mohamad Hossein Askari <askarimohamadhosseinpc@gmail.com>
2025-04-22 15:17:53 +0330

    Defining the main variables and initializing the register button

---
+++ main.js
@@ -0,0 +1,7 @@
+const tableBody = document.getElementById("tableBody");
+const inputs = document.getElementsByClassName("signup");
+const signupBtn = document.getElementById("signupBtn");
+const nameHeader = document.getElementById("nameHeader");
+const rateHeader = document.getElementById("rateHeader");
+
+signupBtn.style.cursor = "pointer";
