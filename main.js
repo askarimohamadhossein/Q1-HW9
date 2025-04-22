abbeb97ad7135a8817db1966b8366ca3934c4efd
Mohamad Hossein Askari <askarimohamadhosseinpc@gmail.com>
2025-04-22 15:18:59 +0330

    Add the ability to add a new row to the table

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ const nameHeader = document.getElementById("nameHeader");
 const rateHeader = document.getElementById("rateHeader");
 
 signupBtn.style.cursor = "pointer";
+
+signupBtn.addEventListener("click", () => {
+  const row = document.createElement("tr");
+
+  for (const input of inputs) {
+    const cell = document.createElement("td");
+    cell.textContent = input.value;
+    row.appendChild(cell);
+  }
+
+  const actionCell = document.createElement("td");
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("class", "delete");
+  deleteButton.textContent = "Delete";
+  deleteButton.style.cursor = "pointer";
+
+  deleteButton.addEventListener("click", () => {
+    row.remove();
+  });
+
+  actionCell.appendChild(deleteButton);
+  row.appendChild(actionCell);
+  tableBody.appendChild(row);
+});
